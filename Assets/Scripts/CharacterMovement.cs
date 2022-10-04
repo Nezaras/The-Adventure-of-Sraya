3b95519c030416f2d63bd56b2c11ff5db7a0486e
ahmdshahal <ahmdshahal29@gmail.com>
2022-10-04 15:29:57 +0700

    Add 3d models and update movement
    Adding character Sraya and Nenek
    Adding character animation
    Update character movement script
    Disable jump

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float playerSpeed = 5.0f;
+    public bool canMove;
+    public bool isMove;
 
     [SerializeField] CharacterController controller;
+
+    private Animator anim;
+
     private Vector3 _playerVelocity;
     private bool _groundedPlayer;
     private float _jumpHeight = 1.0f;
     private float _gravityValue = -9.81f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        canMove = true;
+    }
 
     void Update()
     {
@@ -20,14 +31,33 @@ public class CharacterMovement : MonoBehaviour
             _playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * _playerSpeed);
+        Move();
+        //Jump();
+    }
 
-        if (move != Vector3.zero)
+    void Move()
+    {
+        if (canMove)
         {
-            gameObject.transform.forward = move;
-        }
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            controller.Move(move * Time.deltaTime * playerSpeed);
 
+            if (move != Vector3.zero)
+            {
+                isMove = true;
+                anim.SetFloat("Movement", 0.5f);
+                gameObject.transform.forward = move;
+            }
+            else
+            {
+                isMove = false;
+                anim.SetFloat("Movement", 0f);
+            }
+        }
+    }
+
+    void Jump()
+    {
         // Changes the height position of the player..
         if (Input.GetButtonDown("Jump") && _groundedPlayer)
         {
