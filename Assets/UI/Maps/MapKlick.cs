2fed6191641db2078aa04a6388e11f014b36ac29
Clevoid <akbar.2.rizkyandi@gmail.com>
2022-10-20 00:08:43 +0700

    Add key trigger and Large Map

--- Assets/UI/Maps/MapKlick.cs
+++ Assets/UI/Maps/MapKlick.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MapKlick : MonoBehaviour
 {
+	public GameObject largeMapObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,17 @@ public class MapKlick : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){ // if left button pressed...
-            Debug.Log("Mouse Pressed!");
+            largeMapObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.M)){
+            if(!largeMapObject.activeInHierarchy){
+                largeMapObject.SetActive(true);
+            }else{
+                largeMapObject.SetActive(false);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.M)){ // if left button pressed...
+            largeMapObject.SetActive(false);
         }
     }
 }
